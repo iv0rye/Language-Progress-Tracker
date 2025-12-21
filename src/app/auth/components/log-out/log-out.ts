165be3f5847ffac38004a1d02f9a9bb975121bd3
Ivan Li <ivanli10252006@gmail.com>
2025-12-21 15:52:23 +1100

    Add empty basic authentication components

---
+++ src/app/auth/components/log-out/log-out.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-log-out',
+  imports: [],
+  templateUrl: './log-out.html',
+  styleUrl: './log-out.css',
+})
+export class LogOut {
+
+}
