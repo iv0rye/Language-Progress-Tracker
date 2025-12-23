784239439cd6e56263b529f45427791cc04457d5
Ivan Li <ivanli10252006@gmail.com>
2025-12-24 02:32:18 +1100

    Add password hashing through bcrypt

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = mongoose.Schema({
     username: {
         type: String,
         unique: [true, "This username is taken!"],
-        required: [true, "A username is required!"]
+        required: [true, "A username is required!"],
+        minlength: [3, "Username must be at least 3 characters!"],
+        maxlength: [15, "Username exceeds 15 characters!"]
     },
     email: {
         type: String,
@@ -24,6 +27,14 @@ const userSchema = mongoose.Schema({
         minlength: [8, "Password must be at least 8 characters!"],
         select: false
     }
+})
+
+// Hashing password before saving to database
+userSchema.pre("save", async (next) => {
+    if (!this.isModified('password')) return next();
+
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
 })
 
 module.exports = mongoose.model("User", userSchema);
