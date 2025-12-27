b5d69985ae2280159f7934544f22581a5ae75e42
Ivan Li <ivanli10252006@gmail.com>
2025-12-28 04:40:01 +1100

    Remove log out component import

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,8 @@
 import { Routes } from '@angular/router';
 import { LogIn } from './auth/components/log-in/log-in';
-import { LogOut } from './auth/components/log-out/log-out';
 import { SignUp } from './auth/components/sign-up/sign-up';
 
 export const routes: Routes = [
     { path: 'login', component: LogIn },
-    { path: 'logout', component: LogOut },
     { path: 'sign-up', component: SignUp }
 ];
