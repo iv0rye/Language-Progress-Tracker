b67708dc2cff3f35bb85ecbbe03ee14ef27bd5f1
Ivan Li <ivanli10252006@gmail.com>
2025-12-28 04:36:25 +1100

    Generate 2 layout components for main app and auth pages

---
+++ src/app/layouts/footer-only-layout/footer-only-layout.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-only-layout',
+  imports: [],
+  templateUrl: './footer-only-layout.html',
+  styleUrl: './footer-only-layout.css',
+})
+export class FooterOnlyLayout {
+
+}
