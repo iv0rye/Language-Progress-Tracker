d5f66ec5cc69b95e3bce0c441b454c85c837c99e
Ivan Li <ivanli10252006@gmail.com>
2025-12-28 04:54:19 +1100

    Update routes to use layouts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 import { LogIn } from './auth/components/log-in/log-in';
 import { SignUp } from './auth/components/sign-up/sign-up';
+import { FooterOnlyLayout } from './layouts/footer-only-layout/footer-only-layout';
 
-export const routes: Routes = [
-    { path: 'login', component: LogIn },
-    { path: 'sign-up', component: SignUp }
-];
+export const routes: Routes = [{
+    path: 'auth',
+    component: FooterOnlyLayout,
+    children: [
+      { path: 'login', component: LogIn },
+      { path: 'signup', component: SignUp }
+    ]
+}];
