de80be1476629e65858769b26c02ad5599aa025d
Ivan Li <ivanli10252006@gmail.com>
2026-01-02 16:53:41 +1100

    Add post method to session service and implement in component

--- src/app/timer/components/timer/timer.ts
+++ src/app/timer/components/timer/timer.ts
@@ -4,6 +4,7 @@ import { BehaviorSubject, EMPTY, interval, switchMap } from 'rxjs';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { AsyncPipe } from '@angular/common';
 import { ButtonModule } from "primeng/button";
+import { SessionService } from '../../services/session-service';
 
 @Component({
   selector: 'app-timer',
@@ -17,7 +18,11 @@ import { ButtonModule } from "primeng/button";
 export class Timer {
   session: Session = new Session();
 
+  // TODO: Implement all sessions get feature
+  allSessions: Array<any> = []; 
+
   sessionOngoing: boolean = false;
+  errorMessage: string = "";
 
   seconds: number = 0;
   minutes: number = 0;
@@ -26,7 +31,7 @@ export class Timer {
   private isActive$ = new BehaviorSubject<boolean>(true);
   totalSessionTime$ = new BehaviorSubject<number>(0);
 
-  constructor(private destroyRef: DestroyRef){
+  constructor(private destroyRef: DestroyRef, private sessionService: SessionService){
     this.isActive$.pipe(
       switchMap(active => active ? interval(1000) : EMPTY),
       takeUntilDestroyed(this.destroyRef)
@@ -59,6 +64,17 @@ export class Timer {
   stopSession() {
     this.sessionOngoing = false;
     this.isActive$.next(false);
+    this.totalSessionTime$.next(0);
+    this.resetTime();
+
+    this.sessionService.addSession(this.session).subscribe({
+      next: (data: any) => {
+
+      },
+      error: (err: any) => {
+        this.errorMessage = err;
+      }
+    })
   }
 
   // TEMP!! this is a temporary solution for displaying the time in a human way! i plan to use a pipe or something of the sort :)
--- src/app/timer/services/session-service.ts
+++ src/app/timer/services/session-service.ts
@@ -1,8 +1,20 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Session } from '../../models/session';
+
+const API_URL = "http://localhost:8080/api/session";
+
+const httpOptions = {
+  headers: new HttpHeaders({"Content-Type": "application/json"})
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class SessionService {
-  
+  constructor(private http: HttpClient){}
+
+  addSession(session: Session){
+    return this.http.post(API_URL, session, httpOptions);
+  }
 }
